17362bc3ec53ee7e5bd62a11a3aa2a0856109e97
Mykhailo Karpenko <me@mkarp.co>
2019-05-12 21:06:49 +0200

    Don't return two results to the nREPL client when evaluating cljs

--- src/cljs_test/nrepl_middleware.clj
+++ src/cljs_test/nrepl_middleware.clj
@@ -6,13 +6,14 @@
 (defn wrap-app-reload [reload-opts]
   (fn [handler]
     (fn [msg]
-      (handler msg)
-      (when (and (= (:op msg) "eval")
-                 (s/ends-with? (str (:file msg)) "cljs"))
-        (log "Eval and reload " (:ns msg))
-        (handler {:transport (:transport msg)
-                  :session (:session msg)
-                  :ns (:ns reload-opts)
-                  :op "eval"
-                  :id (uuid)
-                  :code (str "(" (:ns reload-opts) "/" (:fn reload-opts) ")")})))))
+      (if (and (= (:op msg) "eval")
+               (s/ends-with? (str (:file msg)) "cljs"))
+        (do
+          (log "Eval and reload" (:ns msg))
+          (handler {:transport (:transport msg)
+                    :session (:session msg)
+                    :ns (:ns reload-opts)
+                    :op "eval"
+                    :id (uuid)
+                    :code (str "(let [result " (:code msg) "] (" (:ns reload-opts) "/" (:fn reload-opts) ") result)")}))
+        (handler msg)))))
