e2e28d8722f27b173c1274a893095a6599ce98b4
Mykhailo Karpenko <me@mkarp.co>
2019-05-13 22:04:02 +0200

    Evaluate code in its original namespace

--- src/cljs_test/nrepl_middleware.clj
+++ src/cljs_test/nrepl_middleware.clj
@@ -1,6 +1,5 @@
 (ns cljs-test.nrepl-middleware
   (:require [clojure.string :as s]
-            [nrepl.misc :refer [uuid]]
             [cljs-test.logging :refer [log]]))
 
 (defn wrap-app-reload [reload-opts]
@@ -10,10 +9,5 @@
                (s/ends-with? (str (:file msg)) "cljs"))
         (do
           (log "Eval and reload" (:ns msg))
-          (handler {:transport (:transport msg)
-                    :session (:session msg)
-                    :ns (:ns reload-opts)
-                    :op "eval"
-                    :id (uuid)
-                    :code (str "(let [result " (:code msg) "] (" (:ns reload-opts) "/" (:fn reload-opts) ") result)")}))
+          (handler (assoc msg :code (str "(let [result " (:code msg) "] (" (:ns reload-opts) "/" (:fn reload-opts) ") result)"))))
         (handler msg)))))
